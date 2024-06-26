8b3c9621aa07f8264c6c015f2cfd5b21d85bea1b
mrtresz <nhlakaniphotzulu@gmail.com>
2024-06-26 15:40:52 +0200

    mern tutorial initial commit

---
+++ MERN_Tutorial/backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = `mongodb+srv://root:root@cluster0.t6l7fny.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0`;
---
+++ MERN_Tutorial/backend/index.js
@@ -0,0 +1,61 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(200).send("welcome To MERN Stack Tutorial");
+});
+
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Send All required fields: title,author,publishYear",
+      });
+    }
+
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+    return res.status(201).send(book);
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).send({ message: err.message });
+  }
+});
+
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
+
+    return res.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+
+    app.listen(PORT, () => {
+      console.log(`App listening in port ${PORT}`);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
